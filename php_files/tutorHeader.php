3eeed53b5ababce7eb10b2a8936b3a8c808e97bb
rareemi <121254688+rareemi@users.noreply.github.com>
2023-02-06 16:12:00 +0300

    php header

---
+++ php_files/tutorHeader.php
@@ -0,0 +1,26 @@
+<header>
+        <img src = "../images/logo.png" class ="logo" width = "140"  height= "140" alt="logo"  >
+        <nav>
+            <ul class = "nav_links">
+                <li><a href = "/html_files/HomePageTutor.html"> Home</a></li>
+                <li><a href = "#"> Profile</a>
+                    <ul> <!--this for drop-down menu-->
+                        <li><a href = "/html_files/ViewProfileTutor.html"> View</a></li>
+                        <li><a href = "/html_files/EditProfileTutor.html"> Edit</a></li>
+                    </ul> 
+                </li>
+                <li><a href = "/html_files/JobRequestTutor.html"> Requests</a></li>
+                <li><a href = "#"> Jobs </a>
+                    <ul> 
+                        <li><a href = "/html_files/CurrentJobs.html"> Current</a></li>
+                        <li><a href = "/html_files/Previousjobs.html"> Previous</a></li>
+                    </ul> 
+                </li>
+                <li><a href = "#"> Rate & Reviews </a></li>
+                
+          
+
+            </ul>
+        </nav>
+        <p><a class= "out" href="../html_files/index.html">Logout</a></p>
+</header>
