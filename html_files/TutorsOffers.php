f3a79379020a358c649d56b1c601b8a460b78f2b
rareemi <121254688+rareemi@users.noreply.github.com>
2023-02-06 18:55:36 +0300

    footer php

--- html_files/TutorsOffers.php
+++ html_files/TutorsOffers.php
@@ -1,4 +1,13 @@
-<?php include("parentHeader"); ?>
+<?php
+session_start();
+/* require ("../php_files/query.php");
+$requests = get_requests($_SESSION['email']); */
+
+?>
+
+<?php include ("../php_files/parentHeader.php"); ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,42 +18,10 @@
     <link rel ="stylesheet" type="text/css" href = "../css_files/TutorsOffers.css">
 </head>
 <body>
-     <!-- <header>
-        <img src = "../images/logo.png" class ="logo" width = "140"  height= "140" alt="logo"  >
-            <nav>
-                <ul class = "nav_links">
-                    <li><a href = "/html_files/HomePageParent.html"> Home</a></li>
-                    <li><a href = "#"> Profile</a>
-                        <ul>
-                            <li><a href = "/html_files/ViewProfileParent.html"> View</a></li>
-                            <li><a href = "/html_files/EditProfileParent.html"> Edit</a></li>
-                        </ul>
-                    </li>
-                    <li><a href = "#"> Requests</a>
-                        <ul> 
-                            <li><a href = "#"> Post</a></li>
-                            <li><a href = "#"> Edit</a></li>
-                        </ul>
-                    </li> 
-                    <li><a href = "../html_files/RequestOffers.html"> Offers</a></li>
-                    <li><a href = "/html_files/Booking.html"> Booking</a>
-                        <ul>
-                            <li><a href = "/html_files/CurrentBooking.html"> Current</a></li>
-                            <li><a href = "/html_files/PreviousBooking.html"> Previous</a></li>
-                        </ul>
-                    </li>
-                   
-                </ul>
-            </nav>
-            <p><a class= "out" href="../html_files/index.html">Logout</a></p>
-    </header> -->
+  
  
     <h2>Tutor Profile</h2><br><br>
     
-
-
- 
-
 <div> 
 <p class="offer">
     <img src="../images/TutorPic1.png" class="pic" height="190" alt = "Tutor Picture"><br>
@@ -55,55 +32,11 @@
 <br><br>
 <a class ="profile" href="../html_files/ShowTutorProfile.html">Show Profile</a><br><br>
 <br>
-<!-- 
-<a class ="accept" href="#">Accept</a>
-<a class="reject" href="#"> Reject </a>
-<br> -->
+
 </p>
 
 </div>
-<footer> 
-       
-      
-    <p class = "p">
-        <table>
-            <tr>
-         <th><a href="mailto:#" class = "con">ContactUs</a>  </th>
-         <th><a href="aboutUs.html " class ="con">aboutUs</a>  </th>
-         <th> <a href="FAQ.html" class = "con">FAQs</a> </th> 
-            </tr> 
-            </table> <br>
-    
-             <center > 
-    
-              
-                <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-               
-    
-    <a href="https://twitter.com" target="_blank" class = "ionicons">
-                
-        <ion-icon name="logo-twitter"></ion-icon> </a>
-            
-                <a href = "https://whatsapp.com" target="_blank" class = "ionicons">
-                    <ion-icon name="logo-whatsapp"></ion-icon>
-                </a>
-                <a href="https://instagram.com" target="_blank" class = "ionicons">
-                    <ion-icon name="logo-instagram"></ion-icon>
-                </a>
-    
-                <a href="https://snapchat.com" target="_blank" class = "ionicons">
-                    <ion-icon name="logo-snapchat"></ion-icon> <br> <br>
-                </a>
-    
-                &copy; A  Tutorly, 2022
-                </center>
-                
-                 
-       
-            </p>
-    
-        </footer>
+<?php include ("../php_files/footer.php"); ?>
 
 </body>
 </html>
