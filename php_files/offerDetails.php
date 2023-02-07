6beb27c3e7f3d5c12238c5229b7080c274a45aa7
rareemi <121254688+rareemi@users.noreply.github.com>
2023-02-07 23:55:32 +0300

    new page

---
+++ php_files/offerDetails.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tutors Offers</title>
+    <link rel ="stylesheet" type="text/css" href = "../css_files/common.css">
+    <link rel ="stylesheet" type="text/css" href = "../css_files/TutorsOffers.css">
+</head>
+<body>
+    <header>
+        <img src = "../images/logo.png" class ="logo" width = "140"  height= "140" alt="logo"  >
+            <nav>
+                <ul class = "nav_links">
+                    <li><a href = "/html_files/HomePageParent.html"> Home</a></li>
+                    <li><a href = "#"> Profile</a>
+                        <ul>
+                            <li><a href = "/html_files/ViewProfileParent.html"> View</a></li>
+                            <li><a href = "/html_files/EditProfileParent.html"> Edit</a></li>
+                        </ul>
+                    </li>
+                    <li><a href = "#"> Requests</a>
+                        <ul> 
+                            <li><a href = "#"> Post</a></li>
+                            <li><a href = "#"> Edit</a></li>
+                        </ul>
+                    </li> 
+                    <li><a href = "../html_files/RequestOffers.html"> Offers</a></li>
+                    <li><a href = "/html_files/Booking.html"> Booking</a>
+                        <ul>
+                            <li><a href = "/html_files/CurrentBooking.html"> Current</a></li>
+                            <li><a href = "/html_files/PreviousBooking.html"> Previous</a></li>
+                        </ul>
+                    </li>
+                   
+                </ul>
+            </nav>
+            <p><a class= "out" href="../html_files/index.html">Logout</a></p>
+    </header>
+ 
+    <h2>Tutor Profile</h2><br><br>
+    
+
+
+ 
+
+<div> 
+<p class="offer">
+    <img src="../images/TutorPic1.png" class="pic" height="190" alt = "Tutor Picture"><br>
+<label class="nameLabel">Tutor Name: </label><br><label class="Name">Sara Mohammed</label><br>
+<label class="ratingLabel">Rating: </label><br><label class="Rate">4.91</label>
+<br>
+<label class="priceLabel">Offerd Price: </label><br><label class="Price">200SR</label>
+<br><br>
+<a class ="profile" href="../html_files/ShowTutorProfile.html">Show Profile</a><br><br>
+<br>
+<!-- 
+<a class ="accept" href="#">Accept</a>
+<a class="reject" href="#"> Reject </a>
+<br> -->
+</p>
+
+</div>
+<footer> 
+       
+      
+    <p class = "p">
+        <table>
+            <tr>
+         <th><a href="mailto:#" class = "con">ContactUs</a>  </th>
+         <th><a href="aboutUs.html " class ="con">aboutUs</a>  </th>
+         <th> <a href="FAQ.html" class = "con">FAQs</a> </th> 
+            </tr> 
+            </table> <br>
+    
+             <center > 
+    
+              
+                <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+               
+    
+    <a href="https://twitter.com" target="_blank" class = "ionicons">
+                
+        <ion-icon name="logo-twitter"></ion-icon> </a>
+            
+                <a href = "https://whatsapp.com" target="_blank" class = "ionicons">
+                    <ion-icon name="logo-whatsapp"></ion-icon>
+                </a>
+                <a href="https://instagram.com" target="_blank" class = "ionicons">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </a>
+    
+                <a href="https://snapchat.com" target="_blank" class = "ionicons">
+                    <ion-icon name="logo-snapchat"></ion-icon> <br> <br>
+                </a>
+    
+                &copy; A  Tutorly, 2022
+                </center>
+                
+                 
+       
+            </p>
+    
+        </footer>
+
+</body>
+</html>
