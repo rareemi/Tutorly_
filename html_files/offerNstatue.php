73413abc2142f3112bc0629544472d87eddac345
sqw18 <sarahhajlan8@gmail.com>
2023-02-09 22:40:11 +0300

    offerNstatue

--- html_files/offerNstatue.php
+++ html_files/offerNstatue.php
@@ -36,7 +36,7 @@ $requests = get_requests($_SESSION['email']);
 <body>
   
   <h2> Offers with Status</h2> <br>
-<table class="tableS" border="1"> 
+<table class="tableS" border ="1"> 
 <thead>
   <tr>     <th> </th> <th>Date </th><th> Duration</th> <th> Student Name</th> <th>Subject </th><th> Price</th><th> Status</th></tr>
 </thead>
@@ -71,45 +71,6 @@ $requests = get_requests($_SESSION['email']);
 
        
       
-
-  <p class = "p">
-    <br><br><br><br> <table> 
-          <tr>
-       <th><a href="mailto:#" class = "con">ContactUs</a>  </th>
-       <th><a href="aboutUs.html " class ="con">aboutUs</a>  </th>
-       <th> <a href="#" class = "con">FAQs</a> </th> 
-          </tr> 
-          </table> <br>
-  
-           <center> 
-  
-            
-              <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-             
-  
-  <a href="https://twitter.com" target="_blank" class = "ionicons">
-              
-      <ion-icon name="logo-twitter"></ion-icon> </a>
-          
-              <a href = "https://whatsapp.com" target="_blank" class = "ionicons">
-                  <ion-icon name="logo-whatsapp"></ion-icon>
-              </a>
-              <a href="https://instagram.com" target="_blank" class = "ionicons">
-                  <ion-icon name="logo-instagram"></ion-icon>
-              </a>
-  
-              <a href="https://snapchat.com" target="_blank" class = "ionicons">
-                  <ion-icon name="logo-snapchat"></ion-icon> <br> <br>
-              </a>
-  
-              &copy; A  Tutorly, 2022
-              </center>
-              
-               
-     
-          </p>
-  
-      </footer>
+<?php include("footer.php");?>
 </body>
 </html>
