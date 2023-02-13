8323c2f8c927586fd47c0bec16d92eda02bda3bd
rareemi <121254688+rareemi@users.noreply.github.com>
2023-02-13 12:48:50 +0300

    accept

--- php_files/setAccepted.php
+++ php_files/setAccepted.php
@@ -3,15 +3,16 @@
 
 if(isset($_GET['id'])){
    $id=  $_GET['id'];
-   $name = $_GET['name'];
+   $name = $_GET['n'];
 
    $sql = "UPDATE `requests` SET `status`= 'served' WHERE `ID` = $id ";
-   $sq2 = "UPDATE `offer` SET `offerstatus`='accepted' WHERE `RequestID` = $id AND `tutorName` = '$name'";
+   $sq2 = "UPDATE `offer` SET `offerstatus`='accepted' WHERE `id` = $id ";
    $result1 = mysqli_query($connection,$sql);
    $result1 = mysqli_query($connection,$sq2);
 
    //header("Location: http://localhost/Tutorly_/php_files/RejectOthers.php?id=$id&name=$name");
-   header("Location: http://localhost/Tutorly_/php_files/RequestOffers.php");
+ //  header("Location: http://localhost/Tutorly_/php_files/RequestOffers.php");
+ header("Location: http://localhost/Tutorly_/php_files/RequestOffers.php");
 
    
 }
