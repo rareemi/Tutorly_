5f70e954a5ef5b1b05d4e555aad051f4f210becb
ghosson16 <ghossonmoafa2002@gmail.com>
2023-02-13 15:42:42 +0300

    Update 381.sql

--- SQL/381.sql
+++ SQL/381.sql
@@ -38,10 +38,10 @@ CREATE TABLE `kids` (
 --
 
 INSERT INTO `kids` (`ID`, `kidName`, `kidAge`) VALUES
-(1, 'bassam', 14),
-(2, 'maha', 10),
-(3, 'rama', 8),
-(4, 'Yazan', 6);
+(2, 'MAHA', 10),
+(4, 'YAZAN', 12),
+(27, 'amal', 10),
+(5, 'bassam', 13);
 
 -- --------------------------------------------------------
 
@@ -66,8 +66,11 @@ CREATE TABLE `offer` (
 --
 
 INSERT INTO `offer` (`id`, `price`, `tutorName`, `RequestID`, `offerstatus`, `tutorEmail`, `startDate`, `startTime`, `endTime`) VALUES
-(1, 100, 'hifza', 1, 'accepted', 'Hifza2@gmail.com', '2023-02-07', '00:00:05', '00:00:00'),
-(2, 200, 'RANA', 4, 'pending', 'bro_200@gmail.com', '2023-02-06', '12:30:00', '13:30:00');
+(2, 200, 'RANA', 4, 'accepted', 'bro_200@gmail.com', '2023-02-06', '12:30:00', '13:30:00'),
+(5, 140, 'RANA', 5, 'pending', 'bro_200@gmail.com', '2023-02-21', '13:30:00', '15:30:00'),
+(12, 100, 'Ahmed', 2, 'pending', 'aa2-34@gmail.com', '2023-02-10', '12:30:00', '14:30:00'),
+(17, 100, 'Ahmed', 27, 'accepted', 'aa2-34@gmail.com', '2023-02-22', '14:55:00', '16:55:00'),
+(19, 110, 'RANA', 27, 'accepted', 'bro_200@gmail.com', '2023-02-22', '14:55:00', '16:55:00');
 
 -- --------------------------------------------------------
 
@@ -90,9 +93,10 @@ CREATE TABLE `parent` (
 --
 
 INSERT INTO `parent` (`email`, `password`, `firstName`, `lastName`, `city`, `Location`, `img`) VALUES
-('gh__201@gmail.com', '123456', 'ghada', 'alotaibi', 'riyadh', 'https://goo.gl/maps/VHanMnQ54Zj8Mvbo9', 'TutorPic1.png'),
-('N__2067@gmail.com', 'NN09876', 'nada', 'ahmad', 'Jeddah', 'https://goo.gl/maps/hkSLxuPmskSzFNGH8', ''),
-('ree-901@gmail.com', 'R234567', 'razan', 'alenezi', 'riyadh', 'https://goo.gl/maps/nDmLQZBuLVParwkd8', '');
+('f_0921@gmail.com', 'FF00984', 'Fahad', 'aldossari', 'riyadh', 'https://goo.gl/maps/otUYkrFM1dEfwkQM6', '33.png'),
+('mm-90@gmaial.com', '1234567', 'mohammed', 'MM', 'riyadh', 'https://goo.gl/maps/otUYkrFM1dEfwkQM6', 'defultpico.jpg'),
+('N__2067@gmail.com', 'NN09876', 'nada', 'ahmad', 'Jeddah', 'https://goo.gl/maps/hkSLxuPmskSzFNGH8', '33.png'),
+('ree-901@gmail.com', 'R234567', 'razan', 'alenezi', 'riyadh', 'https://goo.gl/maps/nDmLQZBuLVParwkd8', '33.png');
 
 -- --------------------------------------------------------
 
@@ -117,10 +121,10 @@ CREATE TABLE `requests` (
 --
 
 INSERT INTO `requests` (`TypeOfClass`, `ID`, `status`, `parentEmail`, `startDate`, `startTime`, `endTime`, `comments`, `created_at`) VALUES
-('Biology', 1, 'accepted', 'gh__201@gmail.com', '2023-02-06', '00:00:00', '00:00:00', '', '2023-02-07 13:02:00'),
-('Math', 2, 'served', 'N__2067@gmail.com', '2023-02-10', '12:30:00', '14:30:00', '', '2023-02-07 19:11:28'),
-('English language', 3, 'served', 'ree-901@gmail.com', '2023-02-22', '10:00:00', '12:00:00', '', '2023-02-07 21:25:04'),
-('Math', 4, 'unserved', 'ree-901@gmail.com', '2023-02-06', '09:00:00', '10:00:00', '', '2023-02-08 00:58:23');
+('Math', 2, 'expired', 'N__2067@gmail.com', '2023-02-10', '12:30:00', '14:30:00', '', '2023-02-13 11:58:27'),
+('Math', 4, 'expired', 'ree-901@gmail.com', '2023-02-06', '09:00:00', '10:00:00', '', '2023-02-09 14:15:31'),
+('reading and writing', 5, 'expired', 'N__2067@gmail.com', '2023-02-12', '10:00:00', '15:15:00', '', '2023-02-13 15:08:21'),
+('computer', 27, 'served', 'ree-901@gmail.com', '2023-02-22', '14:55:00', '16:55:00', 'no comment added', '2023-02-13 14:52:52');
 
 -- --------------------------------------------------------
 
@@ -137,6 +141,13 @@ CREATE TABLE `review` (
   `tutorEmail` varchar(100) NOT NULL,
   `offerID` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `review`
+--
+
+INSERT INTO `review` (`Rate`, `feedBack`, `Date`, `time`, `parentEmail`, `tutorEmail`, `offerID`) VALUES
+(5, ' She was a very cool teacher ^-^', '2023-02-09', '16:48', 'ree-901@gmail.com', 'bro_200@gmail.com', 2);
 
 -- --------------------------------------------------------
 
@@ -163,9 +174,10 @@ CREATE TABLE `tutor` (
 --
 
 INSERT INTO `tutor` (`email`, `password`, `firstName`, `lastName`, `gender`, `ID`, `age`, `city`, `phone`, `bio`, `img`) VALUES
-('bro_200@gmail.com', 'BB23456', 'RANA', ' B.', 'female', 467293840, 36, 'Jeddah', '0503999204', 'I have more than 4 years of experience teaching to students from grade 1 to 12. I love science and math and  I make them fun to learn. I am very patient and I would go step by step with you, make sure you understand everything and you are not bored.', ''),
-('Hifza2@gmail.com', 'H1234567', 'Hifza', 'A', 'female', 123456789, 35, 'riyadh', '0555555555', 'Hey there, I\'m Dr. Hifza and I am an experienced Biology and Chemistry tutor. I\'m also proficient in teaching medical subjects (Physiology, Biochemistry Anatomy & Pathology) I can teach any age group from 6 year olds to Adults.\nBiology is a subject I really enjoy teaching and I consider myself', 'TutorPic1.png'),
-('rr_288@gmail.com', 'R12345', 'Reem', 'mohammed', 'female', 234567894, 34, 'riyadh', '0502000000', 'Hi, my name is reem and I’m a certified tutor with a degree in math education. I’ve been tutoring math for the past five years and specialize in all topics of mathematics including calculus, geometry, algebra, and trigonometry. I’m passionate about teaching and helping my students achieve their full potential by providing them with personalized instruction and guidance. I enjoy using technology to make learning more interactive and enjoyable.', '');
+('aa2-34@gmail.com', 'AA09876', 'Ahmed', 'A', 'male', 2147483640, 32, 'riyadh', '0509999939', '                                                                                                                                    My name is Ahmed and I’m a certified tutor with a degree in Computer Science. I specialize in tutoring students of all ages in a variety of topics including computer programming, system analysis, and web development. I’m passionate about helping others understand the power of technology and strive to make learning fun and constructive.                                                                                                                                                                                                ', '11.png'),
+('bro_200@gmail.com', 'BB23456', 'RANA', ' B', 'female', 467293840, 30, 'Jeddah', '0503999204', '                                 I have more than 4 years of experience teaching to students from grade 1 to 12. I love science and math and  I make them fun to learn. I am very patient and I would go step by step with you, make sure you understand everything and you are not bored.                                                ', 'TutorPic1.png'),
+('mm2343@gmail.com', '1234567', 'mohammed', 'MM', 'male', 1234567890, 32, 'riyadh', '0555555767', '           gg                ', 'TutorPic1.png'),
+('rr_288@gmail.com', 'R12345', 'Reem', 'mohammed', 'female', 234567894, 34, 'riyadh', '0502000000', 'Hi, my name is reem and I’m a certified tutor with a degree in math education. I’ve been tutoring math for the past five years and specialize in all topics of mathematics including calculus, geometry, algebra, and trigonometry. I’m passionate about teaching and helping my students achieve their full potential by providing them with personalized instruction and guidance. I enjoy using technology to make learning more interactive and enjoyable.', 'TutorPic1.png');
 
 --
 -- Indexes for dumped tables
@@ -175,7 +187,7 @@ INSERT INTO `tutor` (`email`, `password`, `firstName`, `lastName`, `gender`, `ID
 -- Indexes for table `kids`
 --
 ALTER TABLE `kids`
-  ADD PRIMARY KEY (`ID`);
+  ADD KEY `ID` (`ID`);
 
 --
 -- Indexes for table `offer`
@@ -215,32 +227,20 @@ ALTER TABLE `tutor`
 --
 
 --
--- AUTO_INCREMENT for table `kids`
---
-ALTER TABLE `kids`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `offer`
 --
 ALTER TABLE `offer`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `kids`
---
-ALTER TABLE `kids`
-  ADD CONSTRAINT `kids_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `requests` (`ID`);
 
 --
 -- Constraints for table `offer`
